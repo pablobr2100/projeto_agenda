7e70a42b5639f987ecd8aec1a649881145e125f9
Pablo Ribeiro <pablo__br_13@hotmail.com>
2023-05-27 11:25:50 -0300

    Implementing a Contact CRUD

---
+++ contact/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from contact.models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
+        )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
