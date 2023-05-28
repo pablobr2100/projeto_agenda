204858243a244e73e7c432e6e84c6361d5bc7ebb
Pablo Ribeiro <pablo__br_13@hotmail.com>
2023-05-28 00:40:00 -0300

    Creating User Register Form

--- contact/forms.py
+++ contact/forms.py
@@ -1,15 +1,26 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 from contact.models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
+
     class Meta:
         model = Contact
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture',
         )
 
     def clean(self):
@@ -26,3 +37,33 @@ class ContactForm(forms.ModelForm):
             self.add_error('last_name', msg)
 
         return super().clean()
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+    )
+    last_name = forms.CharField(
+        required=True,
+    )
+    email = forms.EmailField(
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail', code='invalid')
+            )
+
+        return email
