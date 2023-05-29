79abbbb3fe14f4b15f6af7babc16dbfcf7739cc8
Pablo Ribeiro <pablo__br_13@hotmail.com>
2023-05-28 23:48:31 -0300

    Impementing Login, Logout and Update for Users

--- contact/urls.py
+++ contact/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('contact/create/', views.create, name='create'),
 
     path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
 
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
